8fdaeb1c812e2f74c90ed263afb82f727c2cdd06
Dark-Crafter <praoabhay@gmail.com>
2024-12-15 15:33:27 +0530

    added extras

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-
 import { WebSocketServer } from 'ws';
 import { GameManager } from './GameManager';
 
-const wss = new WebSocketServer({ port: 8080 });
+// Convert the port to a number
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
+
+const wss = new WebSocketServer({ port });
 
 const gameManager = new GameManager();
 
 wss.on('connection', function connection(ws) {
-  gameManager.addUser(ws)
-  ws.on("disconnect", () => gameManager.removeUser(ws))
+  gameManager.addUser(ws);
+  ws.on("disconnect", () => gameManager.removeUser(ws));
 });
