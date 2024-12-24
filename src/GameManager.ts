b6014686bd0060d4b7bbba7a965b7065b0149c8a
Dark-Crafter <praoabhay@gmail.com>
2024-12-24 17:57:20 +0530

    added spectator page

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -1,52 +1,112 @@
 import { WebSocket } from "ws";
-import { INIT_GAME, MOVE } from "./messages";
+import { INIT_GAME, MOVE, FETCH_GAMES, GAMES_LIST, JOIN_SPECTATE, GAME_STATE, GAME_UPDATE } from "./messages";
 import { Game } from "./Game";
 
-// User, Game
-
 export class GameManager {
-    private games: Game[];
-    private pendingUser: WebSocket | null;
-    private users: WebSocket[];
+  private games: Game[];
+  private pendingUser: WebSocket | null;
+  private users: WebSocket[];
+  private spectators: Map<string, Set<WebSocket>>;
 
-    constructor() {
-        this.games = [];
-        this.pendingUser = null;
-        this.users = [];
-    }
+  constructor() {
+    this.games = [];
+    this.pendingUser = null;
+    this.users = [];
+    this.spectators = new Map();
+  }
 
-    addUser(socket: WebSocket) {
-        this.users.push(socket);
-        this.addHandler(socket)
-    }
+  addUser(socket: WebSocket) {
+    this.users.push(socket);
+    this.addHandler(socket);
+  }
 
-    removeUser(socket: WebSocket) {
-        this.users = this.users.filter(user => user !== socket);
-        // Stop the game here because the user left
-    }
+  removeUser(socket: WebSocket) {
+    this.users = this.users.filter(user => user !== socket);
+    // Remove from spectators if present
+    this.spectators.forEach((spectators, gameId) => {
+      spectators.delete(socket);
+      if (spectators.size === 0) {
+        this.spectators.delete(gameId);
+      }
+    });
+  }
 
-    private addHandler(socket: WebSocket) {
-        socket.on("message", (data) => {
-            const message = JSON.parse(data.toString());
+  private broadcastGameState(game: Game) {
+    const spectators = this.spectators.get(game.id);
+    if (!spectators) return;
 
-            if (message.type === INIT_GAME) {
-                if (this.pendingUser) {
-                    const game = new Game(this.pendingUser, socket);
-                    this.games.push(game);
-                    this.pendingUser = null;
-                } else {
-                    this.pendingUser = socket;
-                }
+    const gameState = {
+      type: GAME_UPDATE,
+      payload: {
+        fen: game.board.fen(),
+        whiteTime: game.getWhiteTime(),
+        blackTime: game.getBlackTime(),
+        move: game.getLastMove()
+      }
+    };
+
+    spectators.forEach(spectator => {
+      spectator.send(JSON.stringify(gameState));
+    });
+  }
+
+  private addHandler(socket: WebSocket) {
+    socket.on("message", (data) => {
+      const message = JSON.parse(data.toString());
+
+      switch (message.type) {
+        case INIT_GAME:
+          if (this.pendingUser) {
+            const game = new Game(this.pendingUser, socket);
+            this.games.push(game);
+            this.pendingUser = null;
+          } else {
+            this.pendingUser = socket;
+          }
+          break;
+
+        case MOVE:
+          const game = this.games.find(game => 
+            game.player1 === socket || game.player2 === socket
+          );
+          if (game) {
+            game.makeMove(socket, message.payload.move);
+            this.broadcastGameState(game);
+          }
+          break;
+
+        case FETCH_GAMES:
+          const activeGames = this.games.map(game => ({
+            id: game.id,
+            player1: "Player 1",
+            player2: "Player 2",
+            status: game.getStatus()
+          }));
+          socket.send(JSON.stringify({
+            type: GAMES_LIST,
+            payload: { games: activeGames }
+          }));
+          break;
+
+        case JOIN_SPECTATE:
+          const targetGame = this.games.find(g => g.id === message.payload.gameId);
+          if (targetGame) {
+            if (!this.spectators.has(targetGame.id)) {
+              this.spectators.set(targetGame.id, new Set());
             }
-
-            if (message.type === MOVE) {
-                console.log("inside move")
-                const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
-                if (game) {
-                    console.log("inside makemove")
-                    game.makeMove(socket, message.payload.move);
-                }
-            }
-        })
-    }
+            this.spectators.get(targetGame.id)!.add(socket);
+            
+            socket.send(JSON.stringify({
+              type: GAME_STATE,
+              payload: {
+                fen: targetGame.board.fen(),
+                whiteTime: targetGame.getWhiteTime(),
+                blackTime: targetGame.getBlackTime()
+              }
+            }));
+          }
+          break;
+      }
+    });
+  }
 }
--- src/messages.ts
+++ src/messages.ts
@@ -1,3 +1,8 @@
 export const INIT_GAME = "init_game";
-export const MOVE = "move"; 
+export const MOVE = "move";
 export const GAME_OVER = "game_over";
+export const FETCH_GAMES = "fetch_games";
+export const GAMES_LIST = "games_list";
+export const JOIN_SPECTATE = "join_spectate";
+export const GAME_STATE = "game_state";
+export const GAME_UPDATE = "game_update";
